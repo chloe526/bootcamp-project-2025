01fb3b4a8d23805b3185f6fde8998dfb481a1a7d
2chloee <2chloeee@gmail.com>
2025-10-29 20:30:32 -0700

    feat: added content to contact, changed to react

--- bootcamp-milestone-2/src/app/contact/page.tsx
+++ bootcamp-milestone-2/src/app/contact/page.tsx
@@ -1,11 +1,30 @@
 import styles from './../page.module.css'
-import Link from 'next/link'
 
-export default function Contact() {
-  
-    return (
-        <div> 
-            hello world
-        </div>
-  )
-}
+  export default function Contact() {
+        return (
+          <div>
+            <h1 className={styles.pageLogo}>- Contact -</h1>
+            <p>This lists the best way to contact me.</p>
+
+            <p style={{ textAlign: 'center' }}>Email: low_chloe@icloud.com</p>
+
+            <form id="contact-form" className={styles.contactForm}>
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" placeholder="Name" required />
+              <br />
+
+              <label htmlFor="email">Email:</label>
+              <input type="text" id="email" name="email" placeholder="Email" required />
+              <br />
+
+              <label htmlFor="comments">Comments:</label>
+              <textarea id="comments" name="comments" placeholder="Comments" required></textarea>
+              <br />
+
+              <p style={{ textAlign: 'center' }}>
+                <input type="submit" value="Submit" />
+              </p>
+            </form>
+      </div>
+    )
+  }
