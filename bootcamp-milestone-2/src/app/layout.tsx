9a1719c60e9ffd257849485300b7c9c58347529e
2chloee <2chloeee@gmail.com>
2025-10-30 14:11:06 -0700

    chore: removed dead code

--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -1,29 +1,15 @@
-import type { Metadata } from "next"; 
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
-// add footer later
-
 export const metadata: Metadata = {
   title: "[name] Personal Website",
   description: "A personal website for [name].",
-}; 
+};
 
 export default function RootLayout({
   children,
@@ -33,13 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />  
+        <Navbar />
         {children}
-      </body>
 
-      <footer>
-        <Footer />
-      </footer>
+        <footer>
+          <Footer />
+        </footer>
+      </body>
     </html>
   );
 }
