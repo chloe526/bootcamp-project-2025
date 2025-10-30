f7fcbed9158445ce287ed0a0d5eae79625ecfc03
2chloee <2chloeee@gmail.com>
2025-10-30 14:18:12 -0700

    feat: added footer component

--- bootcamp-milestone-2/src/components/footer.tsx
+++ bootcamp-milestone-2/src/components/footer.tsx
@@ -1,13 +1,12 @@
     import styles from './navbar.module.css'
-    import Link from 'next/link'
 
     export default function Footer() {
     return (
         <div>
         <br />
-        <footer className={styles.footer}>
+            <footer className={styles.footer}>
             © 2023 Chloe Low&apos;s Website!! | All Rights Reserved
-        </footer>
+            </footer>
         </div>
     )
     }
