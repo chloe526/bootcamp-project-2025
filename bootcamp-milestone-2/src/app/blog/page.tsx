89b61f7a2aec9308ed335707b8d7a48bb406c333
2chloee <2chloeee@gmail.com>
2025-10-30 14:12:29 -0700

    feat: removed button

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -1,38 +1,45 @@
-import styles from "./../page.module.css";
-import BlogPreview from "../../components/blogPreview";
-import blogs from "@/app/blogData";
+import styles from './../page.module.css'
+import BlogPreview from '../../components/blogPreview';
+import blogs from '@/app/blogData'
 
 export default function Blog() {
-  return (
+    return (
     <div>
-      <main>
-        <h1 className={styles.pageLogo}>- Blog -</h1>
+        <main className={styles.monospace}>
+            <h1 className={styles.pageLogo}>- Blog -</h1>
 
-        <p style={{ textAlign: "center" }}>
-          Welcome to the blog! Click on the links to read more.
-        </p>
+            <p style={{ textAlign: 'center' }}>
+            Welcome to the blog! Click on the links to read more.
+            </p>
 
-        <div className={styles.blogContainer}>
-          {blogs.map((blog) => (
-            <BlogPreview
-              title={blog.title}
-              date={blog.date}
-              description={blog.description}
-              image={blog.image}
-              imageAlt={blog.imageAlt}
-              slug={blog.slug}
-              key={blog.id}
-              id={blog.id}
-            />
-          ))}
-        </div>
+            <div className={styles.blogContainer}>
+                {
+                blogs.map(blog => 
+                    
+                    <BlogPreview
+                        title={blog.title}
+                        date={blog.date}
+                        description={blog.description}
+                        image={blog.image}
+                        imageAlt={blog.imageAlt}
+                        slug={blog.slug}
+                        key={blog.id}
+                        id = {blog.id}
+                    />
+                )}
+            </div>
 
-        <br />
-        <div className={styles.buttonContainer}>
-          <button className={styles.pretty}>click me click me</button>
-        </div>
-      </main>
-      <br />
+            <br/>
+
+            <br/>
+            {/* <div className={styles.buttonContainer}>
+            <button className={styles.pretty}>
+                click me click me
+            </button>
+            </div> */}
+
+            <br/>
+        </main>
     </div>
-  );
+  )
 }
