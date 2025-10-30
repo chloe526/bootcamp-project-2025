5b785ba0d99ab7c4653ffada4c313f597ec0b5d6
2chloee <2chloeee@gmail.com>
2025-10-30 14:21:00 -0700

    feat: changed styling for blog preview

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -1,41 +1,39 @@
-    import React from 'react';
-    import Image from 'next/image';
-    import styles from './blogPreview.module.css';
+import Image, { StaticImageData } from "next/image";
+import styles from "./blogPreview.module.css";
+import Link from 'next/link'
 
-    interface BlogPreviewProps {
-        title: string; // done
-        date: string; // done
-        description: string; // done
-        image: string; // done
-        imageAlt: string; // done
-        slug: string; // done
-        id: string; // done
-    }
+interface BlogPreviewProps {
+  title: string; 
+  date: string; 
+  description: string;
+  image: string | StaticImageData; 
+  imageAlt: string; 
+  slug: string;
+  id: string;
+}
 
-    export default function BlogPreview(params : BlogPreviewProps) {
-    return (
+export default function BlogPreview(params: BlogPreviewProps) {
+  return (
+    <div className={styles.blogCard} id={params.id}>
+      <Image
+        src={params.image}
+        alt={params.imageAlt}
+        width={500}
+        height={300}
+        className={styles.blogImage}
+      />
 
+      <h3 className={styles.blogTitle}>{params.title}</h3>
+
+      <div className={styles.blogText}>
+        <p className={styles.blogDescription}>{params.description}</p>
+        <p className={styles.blogPosted}>Posted on: {params.date}</p>
+        <Link href={`/${params.slug}`} className={styles.blogLink}>
+          Read more
+        </Link>
+        
+      </div>
     
-        <div className={styles.blogCard} id={params.id}>
-            <Image
-                src={params.image}
-                alt={params.imageAlt}
-                width={500}
-                height={300}
-                className={styles.blogImage}
-            />
-            
-            <h3 className={styles.blogTitle}>{params.title}</h3>
-            
-            <div>
-                <p className={styles.blogDescription}>{params.description}</p>
-                <p className={styles.blogDate}>Posted on: {params.date}</p>
-                
-            </div>
-
-            {/* <Link href={"${params.slug}.html"} className={styles.blogLink}>
-                Read more
-            </Link> */}
-        </div>
-    );
-    }
+    </div>
+  );
+}
