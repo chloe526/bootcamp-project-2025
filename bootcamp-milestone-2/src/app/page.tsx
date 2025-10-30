8a8db48e3019421699ba4dc69a9fd86cfed30d79
2chloee <2chloeee@gmail.com>
2025-10-30 14:11:47 -0700

    feat: imported image instead of direct source

--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -1,25 +1,31 @@
 import styles from './page.module.css'
+import mango from '../public/mango.jpg'
+import Image from "next/image";
 
 export default function Home() {
   
     return (
-        <main>
+        <main className={styles.monospace}>
             <h1 className={styles.pageLogo}>- Home Page -</h1>
             <br />
+            
             <div className={styles.aboutImage}>
-            <img src="@/public/mango.jpg" alt="mango logo" />
+            <Image
+              src={mango}
+              alt="mango"> 
+            </Image>
             </div>
+            
             <br />
             <div className={styles.aboutText}>
             <hr />
             <p style={{ textAlign: 'center' }}>
-                <strong>Chloe Low</strong> is a 1st year at Calpoly SLO majoring in Computer Science and plans to minor <br /> 
-                in bioinformatics. In her free time she loves to play violin, dance, and go out to eat with friends.
+                <strong>Chloe Low</strong> is a 1st year at Calpoly SLO majoring in Computer Science. <br /> In her free time she loves to play violin, dance, and go out to eat with friends.
                 <br />
                 She’s currently taking CSC 202, Public Speaking, Calc III, American Government and History, <br />
                 and CSC490.
             </p>
             </div>
-        </main>
+       </main>
   )
 }
