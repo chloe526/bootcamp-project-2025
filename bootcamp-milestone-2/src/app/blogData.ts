fe33eb820f72e40791fd856a5927a30abb56952b
2chloee <2chloeee@gmail.com>
2025-10-29 21:16:46 -0700

    feat: fixed pathway for images in blog

--- bootcamp-milestone-2/src/app/blogData.ts
+++ bootcamp-milestone-2/src/app/blogData.ts
@@ -1,9 +1,11 @@
+import dead from '../public/dead.jpg'
+
 const blogs = [
         {
         title: "Midterm Season",
         date: "October 16, 2025",
         description: "Day in life of college student during their first midterm",
-        image: "/public/dead.jpg",
+        image: dead, 
         imageAlt: "crashing out",
         slug: "/hellweek",
         id: "1"
@@ -12,7 +14,7 @@ const blogs = [
         title: "Shawn Mendes Concert",
         date: "October 14, 2025",
         description: "Went back to the bay area to see family and attend Shawn Mendes concert with childhood friends",
-        image: "/public/shawnmendes.jpg",
+        image: "/src/public/shawnmendes.jpg",
         imageAlt: "shawn mendes concert pic",
         slug: "/shawnmendes",
         id: "2"
@@ -21,7 +23,7 @@ const blogs = [
         title: "Food.",
         date: "May 26 2007 - October 16, 2025",
         description: "Food that I've been deprived of since leaving for Calpoly SLO",
-        image: "/public/huilaushan.jpg",
+        image: "/src/public/huilaushan.jpg",
         imageAlt: "hui lau shan preview",
         slug: "food",
         id: "3"
