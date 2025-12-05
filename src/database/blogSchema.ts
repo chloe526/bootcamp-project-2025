5a7ccfe124093d15ca52b1b5fcd2d6dd3d3a6012
2chloee <2chloeee@gmail.com>
2025-12-05 01:06:48 -0800

    feat: add comment schema and added comments array to blog type

--- src/database/blogSchema.ts
+++ src/database/blogSchema.ts
@@ -1,33 +1,42 @@
 import mongoose, { Schema } from "mongoose";
 
-// typescript type (can also be an interface)
-type Blog = {
-        title: string; 
-        date: Date; 
-        description: string;
-        content: string;
-        image: string;
-        imageAlt: string; 
-        slug: string;
-        id: string;
-        comments: Comment[]; // array for comments
+export type IComment = {
+    user: string;
+    comment: string;
+    time: Date;
 };
 
+const CommentSchema = new Schema<IComment>({
+    user: { type: String, required: true },
+    comment: { type: String, required: true },
+    time: { type: Date, default: Date.now }
+});
+
+// typescript type (can also be an interface)
+export type Blog = {
+    title: string; 
+    date: Date; 
+    description: string;
+    content: string;
+    image: string;
+    imageAlt: string; 
+    slug: string;
+    id: string;
+    comments: IComment[]; // array for comments
+};
 
 // mongoose schema 
 const blogSchema = new Schema<Blog>({
-        title: { type: String, required: true },
-        date: { type: Date, required: false, default: new Date()},
-        description: { type: String, required: true },
-        content: { type: String, required: true },
-        image: { type: String, required: true },
-        imageAlt: { type: String, required: true },
-        slug: { type: String, required: true },
-        id: { type: String, required: true },
+    title: { type: String, required: true },
+    date: { type: Date, required: false, default: new Date()},
+    description: { type: String, required: true },
+    content: { type: String, required: true },
+    image: { type: String, required: true },
+    imageAlt: { type: String, required: true },
+    slug: { type: String, required: true },
+    id: { type: String, required: true },
+    comments: { type: [CommentSchema], default: [] },
 })
 
-// defining the collection and model
-const Blog = mongoose.models['blogs'] ||
-    mongoose.model('blogs', blogSchema);
-
+const Blog = mongoose.models['blogs'] || mongoose.model('blogs', blogSchema);
 export default Blog;
