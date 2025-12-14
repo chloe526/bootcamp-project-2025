f28ddbb9aa74ef3b614dca99856fae215bb23cb1
2chloee <2chloeee@gmail.com>
2025-12-14 02:55:43 -0800

    feat: fixed routing for blog[slug]

--- src/app/api/blog/[slug]/comment/route.ts
+++ src/app/api/blog/[slug]/comment/route.ts
@@ -1,14 +1,16 @@
+export const dynamic = "force-dynamic";
+
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
 
 export async function POST(
   req: NextRequest,
-  context: { params: Promise<{ slug: string }> }
+  { params }: { params: { slug: string } }
 ) {
   await connectDB();
 
-  const { slug } = await context.params;
+  const { slug } = params;
   const body = await req.json();
 
   if (!body.comment) {
