14a5e643e85398c20bd84d1e89434bb9986343cd
2chloee <2chloeee@gmail.com>
2025-12-14 10:00:38 -0800

    feat: added force dynamic to page.tsx for blog, blog slug, and api blog

--- src/app/api/blog/[slug]/comment/route.ts
+++ src/app/api/blog/[slug]/comment/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema"
+export const dynamic = "force-dynamic";
 
 export async function POST(
   req: NextRequest,
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import styles from "../page.module.css";
 import BlogPreview from "../../components/blogPreview";
 import connectDB from "@/database/db";
